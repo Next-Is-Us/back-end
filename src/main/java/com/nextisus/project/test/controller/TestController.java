77feed2553778a85acd6618c683746d36a2b7054
MinseoKangQ <kms02171@naver.com>
2024-07-14 17:03:37 -0700

    :sparkles: TestController 작성

---
+++ src/main/java/com/nextisus/project/test/controller/TestController.java
@@ -0,0 +1,34 @@
+package com.nextisus.project.test.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<String> getTest() {
+        return ResponseEntity.ok("GET SUCCESS");
+    }
+
+    @PostMapping
+    public ResponseEntity<String> postTest() {
+        return ResponseEntity.ok("POST SUCCESS");
+    }
+
+    @PutMapping
+    public ResponseEntity<String> putTest() {
+        return ResponseEntity.ok("PUT SUCCESS");
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteTest() {
+        return ResponseEntity.ok("DELETE SUCCESS");
+    }
+}
