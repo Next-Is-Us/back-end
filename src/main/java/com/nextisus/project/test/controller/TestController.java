3a5d7fb0fb22e1cbcc4014f0739036d85da3945f
MinseoKangQ <kms02171@naver.com>
2024-07-18 14:46:24 -0700

    :sparkles: PATCH 테스트 추가

--- src/main/java/com/nextisus/project/test/controller/TestController.java
+++ src/main/java/com/nextisus/project/test/controller/TestController.java
@@ -3,6 +3,7 @@ package com.nextisus.project.test.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,11 @@ public class TestController {
     @PutMapping
     public ResponseEntity<String> putTest() {
         return ResponseEntity.ok("PUT SUCCESS");
+    }
+
+    @PatchMapping
+    public ResponseEntity<String> patchTest() {
+        return ResponseEntity.ok("PATCH SUCCESS");
     }
 
     @DeleteMapping
