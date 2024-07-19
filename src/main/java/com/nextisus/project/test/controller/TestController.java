d1220a08afafe578871ae39ada3466ded04c038d
MinseoKangQ <kms02171@naver.com>
2024-07-18 17:43:46 -0700

    :rocket: CI/CD 테스트

--- src/main/java/com/nextisus/project/test/controller/TestController.java
+++ src/main/java/com/nextisus/project/test/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.nextisus.project.test.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/test")
+@RequiredArgsConstructor
 public class TestController {
 
     @GetMapping
