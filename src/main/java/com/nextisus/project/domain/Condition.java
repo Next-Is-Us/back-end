744cc5774c7d4fba84b712c37226ca5057be46cd
Juye0nLee <dlwndus708@naver.com>
2024-07-21 00:23:31 +0900

    ✨ Condition 엔티티 생성

---
+++ src/main/java/com/nextisus/project/domain/Condition.java
@@ -0,0 +1,57 @@
+package com.nextisus.project.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "`CONDITION`")
+public class Condition {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long conditionId;
+
+    @Column(name = "SLEEP_TIME")
+    private String sleepTime;
+
+    @Column(name = "RECORD", columnDefinition = "TEXT")
+    private String record;
+
+    //안면 홍조
+    @Column(name="BLUSH")
+    private Boolean blush;
+
+    //두통
+    @Column(name="HEADACHE")
+    private Boolean headache;
+
+    //복통
+    @Column(name="STOMACHACHE")
+    private Boolean stomachache;
+
+    //변비
+    @Column(name="CONSTIPATION")
+    private Boolean constipation;
+
+    //근육통
+    @Column(name="MUSCLE")
+    private Boolean muscle;
+
+    //피부 트러블
+    @Column(name="PIMPLE")
+    private Boolean pimple;
+
+    //손발 저림
+    @Column(name="NUMBNESS")
+    private Boolean numbness;
+
+    //오한
+    @Column(name="CHILL")
+    private Boolean chill;
+}
