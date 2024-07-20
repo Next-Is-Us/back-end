90dca67967c65ea8e836e35cd169ca55ddda57b8
JuyeonLee <104489022+Juye0nLee@users.noreply.github.com>
2024-07-21 01:23:11 +0900

    ✨ 상태 API 개발 - Entity 생성 (#11)
    * ✨ Condition 엔티티 생성
    
    * ✨ Condition API 개발을 위한 기본 틀 생성

---
+++ src/main/java/com/nextisus/project/condition/service/ConditionServiceImpl.java
@@ -0,0 +1,10 @@
+package com.nextisus.project.condition.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ConditionServiceImpl implements ConditionService{
+
+}
