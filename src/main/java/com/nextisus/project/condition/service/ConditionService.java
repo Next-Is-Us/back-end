42870dbfb4de24db29ba68d3ccf2f3f82cecb864
JuyeonLee <104489022+Juye0nLee@users.noreply.github.com>
2024-07-22 00:57:21 +0900

    ✨ 상태 API 개발 - 상태 기록 기능 구현  (#24)
    * 🎨 몸 상태 변수명 변경
    
    * ✨ 오늘의 상태 기록 코드 작성(테스트 아직)
    
    * ✨ 오늘의 상태 기록 기능 구현
    
    * 🚑️ Application.yml active local->deploy로 바꿈

--- src/main/java/com/nextisus/project/condition/service/ConditionService.java
+++ src/main/java/com/nextisus/project/condition/service/ConditionService.java
@@ -1,4 +1,10 @@
 package com.nextisus.project.condition.service;
 
+import com.nextisus.project.condition.dto.request.CreateConditionRequestDto;
+import com.nextisus.project.util.response.SuccessResponse;
+import org.springframework.http.ResponseEntity;
+
 public interface ConditionService {
+
+    SuccessResponse<?> createCondition(CreateConditionRequestDto request);
 }
