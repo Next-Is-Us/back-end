b733cc0a65d4194318b61da7a90f780340d572ef
JuyeonLee <104489022+Juye0nLee@users.noreply.github.com>
2024-07-22 00:57:21 +0900

    ✨ 상태 API 개발 - 상태 기록 기능 구현  (#24)
    * 🎨 몸 상태 변수명 변경
    
    * ✨ 오늘의 상태 기록 코드 작성(테스트 아직)
    
    * ✨ 오늘의 상태 기록 기능 구현
    
    * 🚑️ Application.yml active local->deploy로 바꿈

--- src/main/java/com/nextisus/project/condition/controller/ConditionController.java
+++ src/main/java/com/nextisus/project/condition/controller/ConditionController.java
@@ -1,13 +1,24 @@
 package com.nextisus.project.condition.controller;
 
+import com.nextisus.project.condition.dto.request.CreateConditionRequestDto;
+import com.nextisus.project.condition.service.ConditionService;
+import com.nextisus.project.util.response.SuccessResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/condition")
+@RequestMapping("/api/test/condition")
 @RequiredArgsConstructor
 public class ConditionController {
 
+    private final ConditionService conditionService;
+
+    // 오늘의 상태 기록
+    @PostMapping
+    public SuccessResponse<CreateConditionRequestDto> createCondition(@Valid @RequestBody CreateConditionRequestDto request) {
+        conditionService.createCondition(request);
+        return SuccessResponse.empty();
+    }
 }
