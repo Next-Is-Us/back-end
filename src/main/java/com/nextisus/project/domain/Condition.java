5ae32baf0f4ce372596cf7ecaca2a65cb0746d92
JuyeonLee <104489022+Juye0nLee@users.noreply.github.com>
2024-07-21 14:49:49 +0900

    ✨ BaseEntity 생성 (#21)

--- src/main/java/com/nextisus/project/domain/Condition.java
+++ src/main/java/com/nextisus/project/domain/Condition.java
@@ -1,5 +1,6 @@
 package com.nextisus.project.domain;
 
+import com.nextisus.project.util.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,7 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "`CONDITION`")
-public class Condition {
+public class Condition extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long conditionId;
