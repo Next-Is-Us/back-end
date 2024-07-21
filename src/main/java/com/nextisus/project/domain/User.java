5662fea9c45625c2aa06bdc189d792c317ff99b1
MinseoKangQ <kms02171@naver.com>
2024-07-21 16:23:48 -0700

    :card_file_box: User와 Role 엔티티 작성

--- src/main/java/com/nextisus/project/domain/User.java
+++ src/main/java/com/nextisus/project/domain/User.java
@@ -1,10 +1,17 @@
 package com.nextisus.project.domain;
 
+import com.nextisus.project.util.entity.BaseEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.List;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,11 +21,45 @@ import lombok.NoArgsConstructor;
 @Table(name = "USERS")
 @Getter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class User {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Builder.Default
+    @Column(name = "is_notification_enabled")
+    private Boolean isNotificationEnabled = true;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<UserRole> userRoles;
+
+    /**
+     *     public static User toEntity(SignUpDto dto, List<Role> roles) {
+     *
+     *         User user = User.builder()
+     *                 .userId(dto.getUserId())
+     *                 .password(dto.getPassword())
+     *                 .email(dto.getEmail())
+     *                 .build();
+     *
+     *         List<UserRole> userRoles = dto.getUserRoles().stream()
+     *                 .map(roleName -> roles.stream()
+     *                         .filter(role -> role.getRoleName().name().equals(roleName))
+     *                         .findFirst()
+     *                         .map(role -> new UserRole(null, user, role))
+     *                         .orElseThrow(() -> new IllegalArgumentException(roleName + "는 존재하지 않는 역할입니다.")))
+     *                 .collect(Collectors.toList());
+     *
+     *         user.userRoles = userRoles;
+     *
+     *         return user;
+     *     }
+     */
 }
