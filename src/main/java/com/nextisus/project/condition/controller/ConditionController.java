67377bc71b22c4dab742b7ea1b044757ab19e6e7
JuyeonLee <104489022+Juye0nLee@users.noreply.github.com>
2024-07-22 00:41:22 +0900

    Revert "✨ 상태 API 개발 - 오늘의 상태 기록 기능 구현  (#22)"
    This reverts commit 7fd352e70c9c6649c500902e43d8f215ea96808d.

--- src/main/java/com/nextisus/project/condition/controller/ConditionController.java
+++ src/main/java/com/nextisus/project/condition/controller/ConditionController.java
@@ -1,24 +1,13 @@
 package com.nextisus.project.condition.controller;
 
-import com.nextisus.project.condition.dto.request.CreateConditionRequestDto;
-import com.nextisus.project.condition.service.ConditionService;
-import com.nextisus.project.util.response.SuccessResponse;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/test/condition")
+@RequestMapping("api/condition")
 @RequiredArgsConstructor
 public class ConditionController {
 
-    private final ConditionService conditionService;
-
-    // 오늘의 상태 기록
-    @PostMapping
-    public SuccessResponse<CreateConditionRequestDto> createCondition(@Valid @RequestBody CreateConditionRequestDto request) {
-        conditionService.createCondition(request);
-        return SuccessResponse.empty();
-    }
 }
