a636acb0a18c7c633e11cdabe0ba6aff315207ac
Minseo Kang <kms02171@naver.com>
2024-07-20 19:55:25 -0700

    ✨ Swagger 추가 (#13)
    * :heavy_plus_sign: Swagger 의존성 추가
    
    * :lock: Swagger 관련 Security 설정 변경
    
    swagger 페이지 접근 가능, 모두가 접근 가능
    
    * :truck: SecurityConfig 클래스 이동
    
    * :wrench: SwaggerConfig 클래스 추가
    
    추후에 JWT 관련 부분 추가 필요
    
    * :sparkles: TestController의 API에 Swagger 작성
    
    * :fire: 필요없는 API 삭제
    
    * :rocket: 설정 profile을 deploy로 변경

---
+++ src/main/java/com/nextisus/project/util/config/SecurityConfig.java
@@ -0,0 +1,85 @@
+package com.nextisus.project.util.config;
+
+import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * 로컬 : http://localhost:8080/swagger-ui/index.html
+ * 서버 : https://15.164.134.131/swagger-ui/index.html
+ */
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    /**
+     * Swagger와 관련된 URI
+     * 아무나 접근 가능
+     * 실 서비스 시에는 막아야 함
+     */
+    private static final String[] SWAGGER_WHITELIST = {
+            "swagger-ui.html/**",
+            "swagger-ui/**",
+            "api-docs",
+            "api-docs/**",
+            "api-docs/**",
+            "v3/api-docs/**",
+            "v3/api-docs/swagger-config"
+    };
+
+    /**
+     * TestController와 관련된 URI
+     * 아무나 접근 가능
+     * 실 서비스 시에는 막아야 함
+     */
+    private static final String[] TEST_WHITELIST = {
+            "api/test/**",
+    };
+
+    /**
+     * 모두가 접근 가능한 URI 정의
+     */
+    private static final String[] PERMIT_ALL_URI;
+
+    static {
+        PERMIT_ALL_URI = Arrays.copyOf(SWAGGER_WHITELIST, SWAGGER_WHITELIST.length + TEST_WHITELIST.length);
+        System.arraycopy(TEST_WHITELIST, 0, PERMIT_ALL_URI, SWAGGER_WHITELIST.length, TEST_WHITELIST.length);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(PERMIT_ALL_URI);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                /**
+                 *  최종 배포 서버에서 반영하기
+                 *  로컬에서 테스트 시 방해됨
+                 */
+//                .requiresChannel((channel) -> channel.anyRequest().requiresSecure())
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(PERMIT_ALL_URI)
+                        .permitAll())
+                .build();
+    }
+
+}
