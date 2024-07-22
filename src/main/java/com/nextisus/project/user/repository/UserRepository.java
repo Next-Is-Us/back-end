3ca70a4c859a2a3724b26e29e14c8c97ac9518d1
Minseo Kang <kms02171@naver.com>
2024-07-22 12:40:03 -0700

    ⚡️ Condition과 User 연관관계 설정 (#45)
    근데 이제 실패를 곁들인..
    
    Co-authored-by: Juye0nLee <dlwndus708@naver.com>

--- src/main/java/com/nextisus/project/user/repository/UserRepository.java
+++ src/main/java/com/nextisus/project/user/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.nextisus.project.user.repository;
 
+import com.nextisus.project.condition.exception.ConditionNotFoundException;
+import com.nextisus.project.domain.Condition;
 import com.nextisus.project.domain.Link;
 import com.nextisus.project.domain.User;
 import java.util.Optional;
+
+import com.nextisus.project.user.exception.UserNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +17,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByLinkAndNickname(Link link, String nickname);
     Optional<User> findByLink(Link link);
     Optional<User> findByIdAndLink(Long id, Link link);
+    default User getByUser(Long id) {
+        return findById(id).orElseThrow(UserNotFoundException::new);
+    }
+
 }
