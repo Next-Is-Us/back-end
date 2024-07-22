6d99ae70a65407f8087156e464d44f32c847e0bf
JuyeonLee <104489022+Juye0nLee@users.noreply.github.com>
2024-07-22 13:30:06 +0900

    ✨ 기록한 상태 조회 기능 구현 (#27)
    * ✨ 오늘 날짜의 기록한 상태 조회 기능 구현
    
    * ✨ NotFound 예외처리 추가
    
    * 🎨 오늘의 상태 조회 메소드명 변경
    
    * ✨ 날짜별 상태 조회 기능 구현
    
    * 🎨 Application.yml active deploy로 변경
    
    * 🎨 Condition url 수정

--- src/main/java/com/nextisus/project/condition/controller/ConditionController.java
+++ src/main/java/com/nextisus/project/condition/controller/ConditionController.java
@@ -1,15 +1,15 @@
 package com.nextisus.project.condition.controller;
 
 import com.nextisus.project.condition.dto.request.CreateConditionRequestDto;
+import com.nextisus.project.condition.dto.response.ConditionListResponseDto;
 import com.nextisus.project.condition.service.ConditionService;
 import com.nextisus.project.util.response.SuccessResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/test/condition")
+@RequestMapping("/api/condition")
 @RequiredArgsConstructor
 public class ConditionController {
 
@@ -17,8 +17,22 @@ public class ConditionController {
 
     // 오늘의 상태 기록
     @PostMapping
-    public SuccessResponse<CreateConditionRequestDto> createCondition(@Valid @RequestBody CreateConditionRequestDto request) {
+    public SuccessResponse<?> createCondition(@Valid @RequestBody CreateConditionRequestDto request) {
         conditionService.createCondition(request);
         return SuccessResponse.empty();
+    }
+
+    // 오늘 날짜로 기록한 상태 조회
+    @GetMapping
+    public SuccessResponse<ConditionListResponseDto> getConditionByToday() {
+        ConditionListResponseDto response = conditionService.getConditionByToday();
+        return SuccessResponse.of(response);
+    }
+
+    // 날짜별 상태 조회
+    @GetMapping("/by-date")
+    public SuccessResponse<ConditionListResponseDto> getConditionByDate(@RequestParam String date) {
+        ConditionListResponseDto response = conditionService.getConditionByDate(date);
+        return SuccessResponse.of(response);
     }
 }
---
+++ src/main/java/com/nextisus/project/condition/exception/ConditionErrorCode.java
@@ -0,0 +1,18 @@
+package com.nextisus.project.condition.exception;
+
+import com.nextisus.project.util.exception.BaseErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import static com.nextisus.project.util.constant.StaticValue.NOT_FOUND;
+
+@Getter
+@AllArgsConstructor
+public enum ConditionErrorCode implements BaseErrorCode {
+
+    CONDITION_NOT_FOUND(NOT_FOUND,"CONDITION_404_1","기록한 상태가 존재하지 않습니다");
+
+    private final int httpStatus;
+    private final String code;
+    private final String message;
+}
