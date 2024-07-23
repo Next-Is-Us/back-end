adcd1ba41582664141586a3f65a025c5928f7938
Minseo Kang <kms02171@naver.com>
2024-07-22 23:01:51 -0700

    :memo: `/api/user/signUp` Swagger 작성 (#50)

--- src/main/java/com/nextisus/project/user/dto/SignUpRequestDto.java
+++ src/main/java/com/nextisus/project/user/dto/SignUpRequestDto.java
@@ -1,5 +1,6 @@
 package com.nextisus.project.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -16,10 +17,16 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignUpRequestDto {
+
     @NotNull
+    @Schema(description = "사용자 역할 목록", example = "[\"ROLE_SON\"]", allowableValues = {"ROLE_SON", "ROLE_DAUGHTER", "ROLE_MOM", "ROLE_ADMIN", "ROLE_DOCTOR"})
     private List<String> userRoles;
+
     @NotNull @NotEmpty @NotBlank
+    @Schema(description = "사용자 닉네임", example = "엄마아들")
     private String nickname;
+
     @NotNull @NotEmpty @NotBlank
+    @Schema(description = "링크", example = "a890e53e-7409-4945-bc57-054aea519aca")
     private String link;
 }
