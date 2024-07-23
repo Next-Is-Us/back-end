7447640b109e00024940a0467a6ce0a824c80882
MinseoKangQ <kms02171@naver.com>
2024-07-23 13:27:07 -0700

    :sparkles: `/api/admin/accessToken`은 모두 접근
    `/api/admin`은 ROLE_ADMIN 만 접근 가능

--- src/main/java/com/nextisus/project/admin/controller/AdminController.java
+++ src/main/java/com/nextisus/project/admin/controller/AdminController.java
@@ -1,9 +1,10 @@
 package com.nextisus.project.admin.controller;
 
+import com.nextisus.project.util.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/admin")
-@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 public class AdminController {
 
     /**
@@ -19,8 +19,17 @@ public class AdminController {
      * ex) 게시글 등록 API
      */
 
-    @GetMapping
-    public void hello() {
-        log.info("ADMIN");
+    // 관리자 accessToken 발급
+    @PostMapping("/accessToken")
+    public void createAccessToken() {
+        log.info("ADMIN: createAccessToken");
+    }
+
+    // 관리자가 InfoPost 생성
+    @PostMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    public SuccessResponse<?> createPost() {
+        log.info("ADMIN: createPost");
+        return null;
     }
 }
