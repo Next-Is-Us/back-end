70cb22fed5980fea60a5c29a31dbd1e4b3e5095b
JuyeonLee <104489022+Juye0nLee@users.noreply.github.com>
2024-07-27 02:09:53 +0900

    ✨ InfoPostImg 엔티티 생성 및 이미지 업로드 구현 (#82)
    * ✨ 건강 정보 게시물 이미지 엔티티 생성
    
    * ✨ 건강 정보 게시물 이미지 업로드  기능 구현

--- src/main/java/com/nextisus/project/admin/service/AdminServiceImpl.java
+++ src/main/java/com/nextisus/project/admin/service/AdminServiceImpl.java
@@ -5,8 +5,12 @@ import com.nextisus.project.admin.dto.CreateAccessTokenResponseDto;
 import com.nextisus.project.admin.dto.CreateInfoPostRequestDto;
 import com.nextisus.project.admin.dto.CreateInfoPostResponseDto;
 import com.nextisus.project.domain.InfoPost;
+import com.nextisus.project.domain.InfoPostImg;
 import com.nextisus.project.domain.Role;
 import com.nextisus.project.domain.User;
+import com.nextisus.project.exception.infopost.InfoPostInternalServerException;
+import com.nextisus.project.image.service.S3UploadService;
+import com.nextisus.project.repository.InfoPostImgRepository;
 import com.nextisus.project.repository.InfoPostRepository;
 import com.nextisus.project.repository.RoleRepository;
 import com.nextisus.project.repository.UserRepository;
@@ -16,7 +20,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @Service
@@ -27,6 +35,8 @@ public class AdminServiceImpl implements AdminService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final InfoPostRepository infoPostRepository;
+    private final S3UploadService s3UploadService;
+    private final InfoPostImgRepository infoPostImgRepository;
 
     @Override
     public CreateAccessTokenResponseDto createAccessToken(CreateAccessTokenRequestDto dto) {
@@ -51,17 +61,36 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public CreateInfoPostResponseDto createInfoPost(CreateInfoPostRequestDto dto, Long id) {
+        try{
+            // 관리자 계정 찾기
+            User adminUser = userRepository.getByUser(id);
 
-        // 관리자 계정 찾기
-        User adminUser = userRepository.getByUser(id);
+            // InfoPost 생성
+            InfoPost infoPost = InfoPost.toEntity(dto, adminUser);
+            infoPostRepository.save(infoPost);
 
-        // InfoPost 생성
-        InfoPost infoPost = InfoPost.toEntity(dto, adminUser);
-        infoPostRepository.save(infoPost);
-
-        // 응답
-        return CreateInfoPostResponseDto.builder()
-                .postId(infoPost.getId())
-                .build();
+            //이미지 업로드
+            String imgPath = null;
+            if(dto.getImageUrl() != null && !dto.getImageUrl().isEmpty()) {
+                for(MultipartFile file : dto.getImageUrl()) {
+                    imgPath = s3UploadService.upload(file,"info-image");
+                    InfoPostImg newImage = InfoPostImg.builder()
+                            .infoPost(infoPost)
+                            .url(imgPath)
+                            .build();
+                    newImage.setPost(infoPost);
+                    infoPostImgRepository.save(newImage);
+                }
+            }
+            // 응답
+            return CreateInfoPostResponseDto.builder()
+                    .postId(infoPost.getId())
+                    .build();
+        }
+        catch (DataAccessException dae) {
+            return null;
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
