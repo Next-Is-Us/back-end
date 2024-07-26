40ea80f1df14e6b110a8bfdd56407e3a45dd012b
Minseo Kang <kms02171@naver.com>
2024-07-25 18:49:41 -0700

    ✨ 건강정보 게시물 조회 API 작성 (#74)
    * :globe_with_meridians: 로컬용 Profile 적용
    
    * :art: 게시글 리스트 조회를 위한 PageResponse 클래스 추가
    
    Page 이용
    
    * :sparkles: 게시글 리스트 조회 API 작성
    
    * :recycle: 리스트 조회 API 코드 리팩토링
    
    게시글 생성순으로 조건 추가
    
    * :rocket: 배포용 Deploy 적용

--- src/main/java/com/nextisus/project/all/infopost/controller/AllInfoPostController.java
+++ src/main/java/com/nextisus/project/all/infopost/controller/AllInfoPostController.java
@@ -1,10 +1,14 @@
 package com.nextisus.project.all.infopost.controller;
 
 import com.nextisus.project.all.infopost.dto.GetDetailInfoPostResDto;
+import com.nextisus.project.all.infopost.dto.GetListInfoPostResDto;
 import com.nextisus.project.all.infopost.service.AllInfoPostService;
+import com.nextisus.project.util.response.PageResponse;
 import com.nextisus.project.util.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +23,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class AllInfoPostController {
 
     private final AllInfoPostService allInfoPostService;
+
+    @GetMapping("/list")
+    public SuccessResponse<PageResponse<GetListInfoPostResDto>> getInfoPostList(
+            @PageableDefault(size = 6, page = 0) Pageable pageable
+            ) {
+        PageResponse<GetListInfoPostResDto> res = allInfoPostService.getListInfoPost(pageable);
+        return SuccessResponse.of(res);
+    }
 
     @GetMapping("/{infoPostId}")
     public SuccessResponse<GetDetailInfoPostResDto> getDetailInfoPost(@PathVariable("infoPostId") Long infoPostId) {
