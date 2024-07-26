e00006ab6000da0f63328508cc4f2a3f52dfdac3
Juye0nLee <dlwndus708@naver.com>
2024-07-26 11:51:59 +0900

    ✨ 병원 공공 Api db에 저장

--- src/main/java/com/nextisus/project/util/loder/ApiDataLoader.java
+++ src/main/java/com/nextisus/project/util/loder/ApiDataLoader.java
@@ -1,13 +1,11 @@
-package com.nextisus.project.util.loader;
+package com.nextisus.project.util.loder;
 
 import com.nextisus.project.domain.Hospital;
 import com.nextisus.project.repository.HospitalRepository;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -15,63 +13,93 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.StringReader;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Slf4j
 @Component
 public class ApiDataLoader implements CommandLineRunner {
 
     private final HospitalRepository hospitalRepository;
-    private final RestTemplate restTemplate;
 
-    public ApiDataLoader(HospitalRepository hospitalRepository, RestTemplate restTemplate) {
+    public ApiDataLoader(HospitalRepository hospitalRepository) {
         this.hospitalRepository = hospitalRepository;
-        this.restTemplate = restTemplate;
     }
 
     @Override
     @Transactional
     public void run(String... args) throws Exception {
         String serviceKey = "o3EWDRotdgYjzHFQ8iYGsXMaC43KutGQFNvAXJGKLQe7aPyiXdNJUplheNRqzASQC2W2GwdXXTyjCl8yw%2BGOig%3D%3D";
-        String pageNo = "1";
-        String numOfRows = "10";
-        String encodedServiceKey = URLEncoder.encode(serviceKey, StandardCharsets.UTF_8.toString());
-        String apiURL = "http://apis.data.go.kr/B552657/HsptlAsembySearchService/getHsptlMdcncFullDown?ServiceKey=" + serviceKey + "&pageNo=" + pageNo + "&numOfRows=" + numOfRows;
+        int totalCount = 76470;
+        int pageNo = 1;
+        int numOfRows = 1000;
+        int totalPages = (int) Math.ceil((double) totalCount / numOfRows);
 
-        String response = restTemplate.getForObject(apiURL, String.class);
+        //for (int pageNo = 1; pageNo <= 100; pageNo++) {
+            String apiURL = "http://apis.data.go.kr/B552657/HsptlAsembySearchService/getHsptlMdcncFullDown?ServiceKey=" + serviceKey + "&pageNo=" + pageNo + "&numOfRows=" + numOfRows;
+            try {
+                URL url = new URL(apiURL);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("Content-type", "application/json");
+                System.out.println("Response code: " + conn.getResponseCode());
 
-        if (response.contains("<returnAuthMsg>SERVICE_KEY_IS_NOT_REGISTERED_ERROR</returnAuthMsg>")) {
-            log.error("API 서비스 키 오류: 서비스 키가 등록되지 않았습니다.");
-            return;
-        }
+                BufferedReader rd;
+                if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+                    rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                } else {
+                    rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+                }
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = rd.readLine()) != null) {
+                    sb.append(line);
+                }
+                rd.close();
+                conn.disconnect();
 
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document doc = builder.parse(new InputSource(new StringReader(response)));
+                String xmlResponse = sb.toString();
 
-        doc.getDocumentElement().normalize();
-        NodeList itemList = doc.getElementsByTagName("item");
+                // XML 파싱 및 데이터 추출
+                try {
+                    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder builder = factory.newDocumentBuilder();
+                    Document doc = builder.parse(new InputSource(new StringReader(xmlResponse)));
 
-        for (int i = 0; i < itemList.getLength(); i++) {
-            Element item = (Element) itemList.item(i);
+                    doc.getDocumentElement().normalize();
 
-            String dutyAddr = getTagValue("dutyAddr", item);
-            String dutyName = getTagValue("dutyName", item);
-            String dutyTel = getTagValue("dutyTel1", item);
+                    NodeList itemList = doc.getElementsByTagName("item");
 
-            log.info("주소: {}", dutyAddr);
-            log.info("이름: {}", dutyName);
-            log.info("전화번호: {}", dutyTel);
+                    for (int i = 0; i < itemList.getLength(); i++) {
+                        Element item = (Element) itemList.item(i);
 
-            Hospital hospital = Hospital.builder()
-                    .hospitalName(dutyName)
-                    .hospitalAddress(dutyAddr)
-                    .hospitalTel(dutyTel)
-                    .build();
-            hospitalRepository.save(hospital);
-        }
+                        String dutyAddr = getTagValue("dutyAddr", item);
+                        String dutyName = getTagValue("dutyName", item);
+                        String dutyTel = getTagValue("dutyTel1", item);
+
+                        /*log.info("주소: {}", dutyAddr);
+                        log.info("이름: {}", dutyName);
+                        log.info("전화번호: {}", dutyTel);*/
+
+                        Hospital hospital = Hospital.builder()
+                                .hospitalName(dutyName)
+                                .hospitalAddress(dutyAddr)
+                                .hospitalTel(dutyTel)
+                                .build();
+                        hospitalRepository.save(hospital);
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+       // }
     }
 
     private static String getTagValue(String tag, Element element) {
