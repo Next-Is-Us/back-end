f2e5a0d5fe297b1d082922794f20fd61f5b7b958
MinseoKangQ <kms02171@naver.com>
2024-07-25 18:30:58 -0700

    :sparkles: 게시글 리스트 조회 API 작성

--- src/main/java/com/nextisus/project/all/infopost/service/AllInfoPostServiceImpl.java
+++ src/main/java/com/nextisus/project/all/infopost/service/AllInfoPostServiceImpl.java
@@ -1,10 +1,16 @@
 package com.nextisus.project.all.infopost.service;
 
 import com.nextisus.project.all.infopost.dto.GetDetailInfoPostResDto;
+import com.nextisus.project.all.infopost.dto.GetListInfoPostResDto;
 import com.nextisus.project.domain.InfoPost;
 import com.nextisus.project.repository.InfoPostRepository;
+import com.nextisus.project.util.response.PageResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -13,6 +19,15 @@ import org.springframework.stereotype.Service;
 public class AllInfoPostServiceImpl implements AllInfoPostService {
 
     private final InfoPostRepository infoPostRepository;
+
+    @Override
+    public PageResponse<GetListInfoPostResDto> getListInfoPost(Pageable pageable) {
+        Page<GetListInfoPostResDto> posts = infoPostRepository.findAll(pageable).map(GetListInfoPostResDto::from);
+        List<GetListInfoPostResDto> list = posts.stream().toList();
+        PageImpl<GetListInfoPostResDto> data = new PageImpl<>(list, pageable,
+                posts.getTotalElements());
+        return PageResponse.of(data);
+    }
 
     @Override
     public GetDetailInfoPostResDto getDetailInfoPost(Long infoPostId) {
