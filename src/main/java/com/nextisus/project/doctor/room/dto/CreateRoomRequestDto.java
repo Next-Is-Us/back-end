b836bd7d26da2c981a9036e33725aa14989f5298
Minseo Kang <kms02171@naver.com>
2024-07-28 14:00:47 -0700

    ✨ 소통방 입장 API 작성  (#94)
    * :art: 소통방 입장 API 작성을 위해 Controller와 Service 작성
    
    * :art: API 작성 진행중
    
    * :construction: NFT 개수 확인 로직 추가
    
    * :lock: 권한 추가
    
    * :bug: 썸네일 없는 경우에도 데이터 저장 가능하도록 수정
    
    * :sparkles: NTF 개수 부족 Exception 적용
    
    * :sparkles: 연관관계 맺기
    
    * :rocket: 배포 Profile 적용

--- src/main/java/com/nextisus/project/doctor/room/dto/CreateRoomRequestDto.java
+++ src/main/java/com/nextisus/project/doctor/room/dto/CreateRoomRequestDto.java
@@ -1,5 +1,7 @@
 package com.nextisus.project.doctor.room.dto;
 
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
@@ -7,8 +9,12 @@ import org.springframework.web.multipart.MultipartFile;
 @Getter
 @Builder
 public class CreateRoomRequestDto {
+    @NotNull
     private String name;
+    @Nullable
     private String introduction;
+    @Nullable
     private Long necessaryNftCount;
+    @Nullable
     private MultipartFile thumbnail;
 }
--- src/main/java/com/nextisus/project/exception/room/RoomErrorCode.java
+++ src/main/java/com/nextisus/project/exception/room/RoomErrorCode.java
@@ -1,5 +1,6 @@
 package com.nextisus.project.exception.room;
 
+import static com.nextisus.project.util.constant.StaticValue.BAD_REQUEST;
 import static com.nextisus.project.util.constant.StaticValue.DUPLICATED;
 import static com.nextisus.project.util.constant.StaticValue.NOT_FOUND;
 
@@ -10,6 +11,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum RoomErrorCode implements BaseErrorCode {
+    ROOM_NFT_NOT_ENOUGH(BAD_REQUEST, "ROOM_400_1", "NFT 개수가 부족합니다."),
     ROOM_NOT_FOUND(NOT_FOUND,"ROOM_404_1","소통방을 찾을 수 없습니다."),
     ROOM_NAME_DUPLICATED(DUPLICATED, "ROOM_409_1", "이미 사용중인 소통방 이름입니다.");
 
