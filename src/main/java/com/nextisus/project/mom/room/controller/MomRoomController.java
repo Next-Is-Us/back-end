41c9553ec8271627657fbef08e8a0346149e8a43
MinseoKangQ <kms02171@naver.com>
2024-07-28 16:36:13 -0700

    :sparkles: Controller 작성

--- src/main/java/com/nextisus/project/mom/room/controller/MomRoomController.java
+++ src/main/java/com/nextisus/project/mom/room/controller/MomRoomController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,12 +34,19 @@ public class MomRoomController {
     }
 
     @GetMapping("/list")
-    @PreAuthorize("hasAnyRole('ROLE_MOM', 'ROLE_ADMIN')")
+    @PreAuthorize("hasAnyRole('ROLE_MOM','ROLE_DOCTOR', 'ROLE_ADMIN')")
     public SuccessResponse<PageResponse<GetRoomListResponseDto>> getRoomList(
             @PageableDefault(size = 4, page = 0) Pageable pageable
             ) {
         long userId = Long.parseLong(authUtil.getCurrentUserId());
         PageResponse<GetRoomListResponseDto> res = momRoomService.getRoomList(userId, pageable);
+        return SuccessResponse.of(res);
+    }
+
+    @GetMapping("detail/{roomId}")
+    @PreAuthorize("hasAnyRole('ROLE_MOM', 'ROLE_DOCTOR', 'ROLE_ADMIN')")
+    public SuccessResponse<GetRoomDetailResponseDto> getRoomDetail(@PathVariable("roomId") Long roomId) {
+        GetRoomDetailResponseDto res = momRoomService.getRoomDetail(roomId);
         return SuccessResponse.of(res);
     }
 
