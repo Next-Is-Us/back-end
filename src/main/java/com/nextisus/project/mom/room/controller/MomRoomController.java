3f8e503c9c6970fb41ed358682791d64082c3e8b
MinseoKangQ <kms02171@naver.com>
2024-07-28 12:58:45 -0700

    :art: API 작성 진행중

--- src/main/java/com/nextisus/project/mom/room/controller/MomRoomController.java
+++ src/main/java/com/nextisus/project/mom/room/controller/MomRoomController.java
@@ -1,7 +1,12 @@
 package com.nextisus.project.mom.room.controller;
 
+import com.nextisus.project.mom.room.dto.EnterRoomRequestDto;
 import com.nextisus.project.mom.room.service.MomRoomService;
+import com.nextisus.project.util.auth.AuthUtil;
+import com.nextisus.project.util.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class MomRoomController {
 
     private final MomRoomService momRoomService;
+    private final AuthUtil authUtil;
+
+    @PostMapping("/enter")
+    public SuccessResponse<?> enterRoom(@RequestBody EnterRoomRequestDto dto) {
+        long userId = Long.parseLong(authUtil.getCurrentUserId());
+        momRoomService.enterRoom(userId, dto);
+        return SuccessResponse.empty();
+    }
 
 }
--- src/main/java/com/nextisus/project/mom/room/service/MomRoomServiceImpl.java
+++ src/main/java/com/nextisus/project/mom/room/service/MomRoomServiceImpl.java
@@ -1,5 +1,6 @@
 package com.nextisus.project.mom.room.service;
 
+import com.nextisus.project.mom.room.dto.EnterRoomRequestDto;
 import com.nextisus.project.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,4 +11,13 @@ public class MomRoomServiceImpl implements MomRoomService {
 
     private final RoomRepository roomRepository;
 
+    @Override
+    public void enterRoom(Long userId, EnterRoomRequestDto dto) {
+
+        // 꽃피 개수 부족한지 확인
+
+        // 이미 입장해 있는지 확인
+
+        // 성공
+    }
 }
