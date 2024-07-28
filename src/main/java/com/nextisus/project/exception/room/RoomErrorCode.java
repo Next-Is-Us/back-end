44a1ce2f7e9256052320db5271f7ef1a2f858db3
MinseoKangQ <kms02171@naver.com>
2024-07-28 13:17:38 -0700

    :lock: 권한 추가

--- src/main/java/com/nextisus/project/exception/room/RoomErrorCode.java
+++ src/main/java/com/nextisus/project/exception/room/RoomErrorCode.java
@@ -11,7 +11,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum RoomErrorCode implements BaseErrorCode {
-    ROOM_NTF_NOT_ENOUGH(BAD_REQUEST, "ROOM_400_1", "NFT 개수가 부족합니다."),
+    ROOM_NFT_NOT_ENOUGH(BAD_REQUEST, "ROOM_400_1", "NFT 개수가 부족합니다."),
     ROOM_NOT_FOUND(NOT_FOUND,"ROOM_404_1","소통방을 찾을 수 없습니다."),
     ROOM_NAME_DUPLICATED(DUPLICATED, "ROOM_409_1", "이미 사용중인 소통방 이름입니다.");
 
--- src/main/java/com/nextisus/project/mom/room/controller/MomRoomController.java
+++ src/main/java/com/nextisus/project/mom/room/controller/MomRoomController.java
@@ -5,6 +5,7 @@ import com.nextisus.project.mom.room.service.MomRoomService;
 import com.nextisus.project.util.auth.AuthUtil;
 import com.nextisus.project.util.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ public class MomRoomController {
     private final AuthUtil authUtil;
 
     @PostMapping("/enter")
+    @PreAuthorize("hasAnyRole('ROLE_MOM', 'ROLE_ADMIN')")
     public SuccessResponse<?> enterRoom(@RequestBody EnterRoomRequestDto dto) {
         long userId = Long.parseLong(authUtil.getCurrentUserId());
         momRoomService.enterRoom(userId, dto);
