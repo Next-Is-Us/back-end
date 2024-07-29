22a3fdbc4b02a084c59314429ffb4cc3a76572ee
Juye0nLee <dlwndus708@naver.com>
2024-07-29 23:52:33 +0900

    🚧 댓글 카운트 하는중

--- src/main/java/com/nextisus/project/repository/RoomCommentRepository.java
+++ src/main/java/com/nextisus/project/repository/RoomCommentRepository.java
@@ -2,13 +2,14 @@ package com.nextisus.project.repository;
 
 import com.nextisus.project.domain.Room;
 import com.nextisus.project.domain.RoomComment;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface RoomCommentRepository extends CrudRepository<RoomComment, Long> {
+public interface RoomCommentRepository extends JpaRepository<RoomComment, Long> {
     List<RoomComment> findAllByRoomPost_Id(Long roomPostId);
-//    Long CountByRoomPost_Id(Long roomPostId);
+    Long CountByRoomPost_Id(Long roomPostId);
 }
