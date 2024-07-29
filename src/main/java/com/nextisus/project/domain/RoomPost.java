a3c15b2caaeb28dbc1b9fcc47bba908a6d92cd46
MinseoKangQ <kms02171@naver.com>
2024-07-28 17:25:30 -0700

    :card_file_box: RoomPost 엔티티 작성

---
+++ src/main/java/com/nextisus/project/domain/RoomPost.java
@@ -0,0 +1,35 @@
+package com.nextisus.project.domain;
+
+import com.nextisus.project.util.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ROOMPOSTS")
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class RoomPost extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content", columnDefinition = "TEXT")
+    private String content;
+
+}
