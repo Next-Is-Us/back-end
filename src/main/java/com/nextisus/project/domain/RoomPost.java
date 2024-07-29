a0cfbb5cce9a84e85ad7c34082e5b6ab1a56d4a2
MinseoKangQ <kms02171@naver.com>
2024-07-28 17:40:03 -0700

    :card_file_box: 연관관계 설정
    User - RoomPost, Room - RoomPost

--- src/main/java/com/nextisus/project/domain/RoomPost.java
+++ src/main/java/com/nextisus/project/domain/RoomPost.java
@@ -3,9 +3,12 @@ package com.nextisus.project.domain;
 import com.nextisus.project.util.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -14,7 +17,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "ROOMPOSTS")
+@Table(name = "ROOM_POSTS")
 @Builder
 @Getter
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
@@ -32,4 +35,11 @@ public class RoomPost extends BaseEntity {
     @Column(name = "content", columnDefinition = "TEXT")
     private String content;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
