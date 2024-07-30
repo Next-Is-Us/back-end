d8464c82228bf5a2943a860959430116acc11979
MinseoKangQ <kms02171@naver.com>
2024-07-29 22:20:59 -0700

    :wrench: Logback 사용을 위한 설정 파일 추가

---
+++ src/main/java/com/nextisus/project/util/config/LogbackConfig.java
@@ -0,0 +1,19 @@
+package com.nextisus.project.util.config;
+
+import com.nextisus.project.util.logging.LoggingInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class LogbackConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoggingInterceptor loggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
+}
