a30c039def02d7eb8087d9b297ad91de717b40bb
Minseo Kang <kms02171@naver.com>
2024-07-29 21:47:47 -0700

    ✨ 마이페이지 API 구현 (#123)
    * :art: API 개발을 위한 틀 작성
    
    * :construction: API 개발을 위한 API 작성
    
    * :construction: 서비스 인터페이스 작성
    
    * :sparkles: 닉네임 조회 API 작성
    
    * :sparkles: 내 가족 정보 API 작성
    
    * :sparkles: 링크 조회 API 작성
    
    * :bug: 현재 유저는 응답에서 제외
    
    * :bug: 내 가족 정보 오류 해결

--- src/main/java/com/nextisus/project/repository/UserRepository.java
+++ src/main/java/com/nextisus/project/repository/UserRepository.java
@@ -1,11 +1,16 @@
 package com.nextisus.project.repository;
 
 import com.nextisus.project.domain.Link;
+import com.nextisus.project.domain.Role;
 import com.nextisus.project.domain.User;
+import com.nextisus.project.domain.UserRole;
+import java.util.List;
 import java.util.Optional;
 
 import com.nextisus.project.exception.user.UserNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -16,6 +21,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByNickname(String nickname);
     Optional<User> findByLink(Link link);
     Optional<User> findByIdAndLink(Long id, Link link);
+//    List<User> findAllByLinkAndUserRoles(Link link, List<UserRole> userRoles);
+    @Query("SELECT u FROM User u JOIN u.userRoles ur WHERE ur.role IN :roles AND u.link = :link")
+    List<User> findAllByLinkAndRoles(@Param("link") Link link, @Param("roles") List<Role> roles);
     default User getByUser(Long id) {
         return findById(id).orElseThrow(UserNotFoundException::new);
     }
