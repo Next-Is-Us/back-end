446b4d0fd66fcdcae3faec46714ba712eabb4914
JuyeonLee <104489022+Juye0nLee@users.noreply.github.com>
2024-07-30 17:02:41 +0900

    🐛 날짜 별 상태 조회 시 유저의 상태만 가져와지도록 수정  (#140)
    * :art: 응답 데이터 가공 (#108)
    
    * 🐛 소통방 peopleCount 오류 해결 (#109)
    
    * :bug: Room의 peopleCount 초기값을 0으로 설정
    
    의사는 포함하지 않기 때문
    
    * :bug: 방 입장해도 peopleCount 증가하지 않는 오류 해결
    
    * :heavy_plus_sign: S3 이미지 업로드를 속도 개선을 위한 의존성 추가 (#113)
    
    * 🐛 건강기록 조회 오류 수정  (#114)
    
    * 🐛 건강 기록 생성 시 recordPeriod,week,nftCount 추가
    
    * 🐛 건강기록 리스트 조회 시 nftCount도 조회 되도록 수정
    
    * ✨ 의료 기록 PDF 변환 시 사용될 데이터 반환 API 작성 (#117)
    
    * ✨ 소통방 댓글 작성 기능 구현
    
    * ⚡️ 소통방 게시글 - 댓글 연관관계 설정
    
    * 🚧 댓글 전체 리스트 조회(페이징 처리x)
    
    * 🐛 오늘의 상태 날짜별 조회 응답 구조 수정
    
    * 🚧 댓글 카운트 하는중
    
    * ✨ PDF 변환 시 사용될 데이터 반환 API 작성
    
    * ⚡️ 소통방 댓글 전체 리스트 조회 수정 (#119)
    
    * ⚡️ 댓글 조회 Pageable 추가
    
    * 🎨 Deploy -> local 로 변경
    
    * Revert "🎨 Deploy -> local 로 변경"
    
    This reverts commit 4ae53f743e1edfc9270070dba6b64fe660250b48.
    
    * 🎨 Local -> deploy로 변경
    
    * 🐛 상태 조회 API 수정  (#121)
    
    * 🐛 날짜 별 기록한 상태 조회 500 에러 해결
    
    * 🐛 기록한 상태 세부 조회 API 수정\
    
    * 🎨 Local -> deploy 수정
    
    * ✨ 마이페이지 API 구현 (#123)
    
    * :art: API 개발을 위한 틀 작성
    
    * :construction: API 개발을 위한 API 작성
    
    * :construction: 서비스 인터페이스 작성
    
    * :sparkles: 닉네임 조회 API 작성
    
    * :sparkles: 내 가족 정보 API 작성
    
    * :sparkles: 링크 조회 API 작성
    
    * :bug: 현재 유저는 응답에서 제외
    
    * :bug: 내 가족 정보 오류 해결
    
    * ⚡️ 상태 기록 dto 수정 (#126)
    
    * ✨ Logback 추가 (#127)
    
    * :heavy_plus_sign: Logback 의존성과 설정 추가
    
    * :wrench: Logback 사용을 위한 xml 파일 추가
    
    * :wrench: Logback 사용을 위한 설정 파일 추가
    
    * :sparkles: Logback 추가
    
    * :rocket: 배포 Profile 적용
    
    * ⚡️ 상태 기록 dto 수정 (#129)
    
    * 🐛 날짜별 기록 조회 유저 검증 코드 추가  (#135)
    
    * ⚡️ 상태 기록 dto 수정
    
    * 🐛 기록 여부 조회 유저 검증
    
    아니 이거 왜 구현 안함? 미쳣나 나
    
    * 🐛 local -> deploy로 변경  (#137)
    
    * ⚡️ 상태 기록 dto 수정
    
    * 🐛 기록 여부 조회 유저 검증
    
    아니 이거 왜 구현 안함? 미쳣나 나
    
    * 🎨 Local로
    
    * 🎨 Local -> deploy로 수정
    
    * 🎨 Deploy -> local 로 수정
    
    ---------
    
    Co-authored-by: Minseo Kang <kms02171@naver.com>

--- src/main/java/com/nextisus/project/mom/condition/service/ConditionServiceImpl.java
+++ src/main/java/com/nextisus/project/mom/condition/service/ConditionServiceImpl.java
@@ -1,5 +1,7 @@
 package com.nextisus.project.mom.condition.service;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.nextisus.project.all.infopost.service.AllInfoPostServiceImpl;
 import com.nextisus.project.domain.Nft;
 import com.nextisus.project.mom.condition.dto.request.CreateConditionRequestDto;
 import com.nextisus.project.client.condition.dto.response.ConditionListResponseDto;
@@ -33,6 +35,7 @@ public class ConditionServiceImpl implements ConditionService {
     private final UserRepository userRepository;
     private final NftServiceImpl nftServiceImpl;
     private final NftRepository nftRepository;
+    private final AllInfoPostServiceImpl allInfoPostServiceImpl;
 
     //오늘의 상태 기록
     @Override
@@ -103,11 +106,11 @@ public class ConditionServiceImpl implements ConditionService {
     public ConditionListResponseDtoByDate getConditionByDate(Long year, Long month, Long day, Long userId) {
 
         User user = userRepository.getByUser(userId);
-        List<Condition> findConditionByYear = conditionRepository.findByYear(year);
+        List<Condition> conditionsByUser = conditionRepository.findAllByUser_Id(userId);
         String recordDate = null;
         Boolean isRecording = false;
-        for(Condition condition : findConditionByYear) {
-            if(condition.getMonth().equals(month) && condition.getDay().equals(day)) {
+        for(Condition condition : conditionsByUser) {
+            if(condition.getMonth().equals(month) && condition.getDay().equals(day) && condition.getYear().equals(year)) {
                 //날짜 형식 포맷
                 LocalDateTime createAt = condition.getCreateAt();
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
