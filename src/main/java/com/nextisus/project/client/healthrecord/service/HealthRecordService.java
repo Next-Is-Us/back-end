d7cb9124c6709b2b8de1466b4862069727cd048c
JuyeonLee <104489022+Juye0nLee@users.noreply.github.com>
2024-08-02 22:15:36 +0900

    🐛 오류 수정 (#165)
    1. 날짜별 기록 여부 조회 api -> 로직 더 간편하게 수정
    2.날짜별 기록 여부 조회 api -> link, userRole 추가
    3. 건강기록 전체 리스트 api -> 유저가 자녀일 때도 엄마의 기록이 불러와지도록 수정

--- src/main/java/com/nextisus/project/client/healthrecord/service/HealthRecordService.java
+++ src/main/java/com/nextisus/project/client/healthrecord/service/HealthRecordService.java
@@ -11,7 +11,7 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface HealthRecordService {
-    List<HealthRecordListDto> getHealthRecord(Long userId);
+    List<HealthRecordListDto> getHealthRecord(Long userId, String userRole);
     HealthRecord createHealthRecord(Long userId,Long countNft);
     HealthRecordResponseDto getHealthRecordDetail(Long healthRecordId);
 
--- src/main/java/com/nextisus/project/exception/healthrecord/HealthRecordErrorCode.java
+++ src/main/java/com/nextisus/project/exception/healthrecord/HealthRecordErrorCode.java
@@ -11,7 +11,8 @@ import static com.nextisus.project.util.constant.StaticValue.NOT_FOUND;
 @AllArgsConstructor
 public enum HealthRecordErrorCode implements BaseErrorCode {
     HEALTH_RECORD_NOT_FOUND(NOT_FOUND,"HEALTH_RECORD_404_1","해당 건강기록id를 가진 건강기록이 존재하지 않습니다"),
-    PDF_BAD_REQUEST(BAD_REQUEST,"PDF_400","pdf파일을 넣어주세요.");
+    PDF_BAD_REQUEST(BAD_REQUEST,"PDF_400","pdf파일을 넣어주세요."),
+    MOM_NOT_FOUND(NOT_FOUND,"MOM_404","해당 자녀유저의 엄마유저가 존재하지 않습니다.");
     private final int httpStatus;
     private final String code;
     private final String message;
