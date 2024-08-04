bb8b1ac3c10f49cd7094dcc5047eaf4e87bde9d8
MinseoKangQ <kms02171@naver.com>
2024-08-04 11:12:16 -0700

    :sparkles: Link 확인 API 작성

--- src/main/java/com/nextisus/project/client/link/controller/LinkController.java
+++ src/main/java/com/nextisus/project/client/link/controller/LinkController.java
@@ -4,6 +4,8 @@ import com.nextisus.project.client.link.dto.LinkResponseDto;
 import com.nextisus.project.client.link.service.LinkService;
 import com.nextisus.project.util.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +24,14 @@ public class LinkController{
     public SuccessResponse<LinkResponseDto> link() {
         LinkResponseDto res = linkService.link();
         return SuccessResponse.of(res);
+    }
+
+    /**
+     * 링크 존재 확인 API
+     */
+    @GetMapping("/{linkUrl}")
+    public SuccessResponse<Void> isLinkExist(@PathVariable("linkUrl") String linkUrl) {
+        linkService.isLinkExist(linkUrl);
+        return SuccessResponse.empty();
     }
 }
