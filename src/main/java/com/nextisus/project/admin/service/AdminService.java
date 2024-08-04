d58c6c9c28fe2dbe8d4096bce79eeea547684095
Minseo Kang <kms02171@naver.com>
2024-08-03 21:06:38 -0700

    ✨ 이메일 전송 기능 구현(3) (#175)
    * :globe_with_meridians: 로컬 Profile로 변경
    
    * :art: 토큰 발급 API 통일
    
    * :fire: 불필요한 코드 삭제
    
    * :sparkles: 유저 accessToken 발급도 추가
    
    * :art: PDF 저장 API 응답에 PDF 경로 추가
    
    * :sparkles: S3에서 파일을 다운로드하는 로직 작성
    
    * :sparkles: 이메일 전송 API 구현
    
    * :bug: 로컬에 파일 저장 삭제
    
    * :pencil2: 이메일 전송 주소 변경
    
    * :rocket: 배포 Profile로 변경

--- src/main/java/com/nextisus/project/admin/service/AdminService.java
+++ src/main/java/com/nextisus/project/admin/service/AdminService.java
@@ -1,11 +1,8 @@
 package com.nextisus.project.admin.service;
 
-import com.nextisus.project.admin.dto.CreateAccessTokenRequestDto;
-import com.nextisus.project.admin.dto.CreateAccessTokenResponseDto;
 import com.nextisus.project.admin.dto.CreateInfoPostRequestDto;
 import com.nextisus.project.admin.dto.CreateInfoPostResponseDto;
 
 public interface AdminService {
-    CreateAccessTokenResponseDto createAccessToken(CreateAccessTokenRequestDto dto);
     CreateInfoPostResponseDto createInfoPost(CreateInfoPostRequestDto dto, Long id);
 }
